f6a77d39f6b082b7f6d4f06f32b0ccbd975311ad
Pranab Mandal <pranavmandal5372@gmail.com>
2025-01-03 23:34:26 +0530

    Add files via upload

---
+++ 14-Gsap.js
@@ -0,0 +1,28 @@
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:1100,
+    scale:0.5,
+    duration:2,
+    // delay:1,
+    rotate:180,
+    backgroundColor:"#524c24",
+})
+
+tl.to("#box2",{
+    x:1100,
+    scale:0.5,
+    duration:2,
+    // delay:3,
+    rotate:180,
+    backgroundColor:"#dc143c",
+})
+
+tl.to("#box3",{
+    x:1100,
+    scale:0.5,
+    duration:2,
+    // delay:3,
+    rotate:180,
+    backgroundColor:"#1c4b66",
+})
