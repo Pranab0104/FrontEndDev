b88538fd740eab624c3a3c0b885d6e3d8b1f0339
Pranab Mandal <pranavmandal5372@gmail.com>
2025-01-09 15:41:14 +0530

    Add files via upload

---
+++ 19-Gsap.js
@@ -0,0 +1,39 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     duration:1,
+//     delay:1,
+//     rotate:360,
+//     opacity:0
+// })
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     duration:1,
+//     // delay:1,
+//     rotate:360,
+//     opacity:0,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         start:"top 50%",
+//         markers:true
+//     }
+// })
+
+// gsap.from("#page3 #box",{
+//     scale:0,
+//     duration:1,
+//     // delay:1,
+//     rotate:360,
+//     opacity:0,
+//     scrollTrigger:{
+//         trigger:"#page3 #box",
+//         scroller:"body",
+//         start:"top 60%",
+//     }
+// })
